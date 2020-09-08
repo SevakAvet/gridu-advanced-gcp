e94073f027e648b8cfc3c0e711b44735da0c6cbe
Sevak Avetisyan <sevak.avet@gmail.com>
2020-09-08 12:45:52 +0400

    Fix upload_template.sh

--- dataflow-tweets/src/main/resources/upload_template.sh
+++ dataflow-tweets/src/main/resources/upload_template.sh
@@ -2,9 +2,9 @@
 
 mvn compile -Pdataflow-runner exec:java \
      -Dexec.mainClass=com.sevak_avet.LoadTweetsFromPubSub \
-     -Dexec.args="--runner=DataflowRunner
-                  --project=divine-bloom-279918
-                  --stagingLocation=gs://tweets_input/tmp
-                  --templateLocation=gs://tweets_input/templates/load_tweets_from_pub_sub
-                  --region=us-east1
+     -Dexec.args="--runner=DataflowRunner \
+                  --project=divine-bloom-279918 \
+                  --stagingLocation=gs://tweets_input/tmp \
+                  --templateLocation=gs://tweets_input/templates/load_tweets_from_pub_sub \
+                  --region=us-east1 \
                   --gcpTempLocation=gs://tweets_input/tmp"
